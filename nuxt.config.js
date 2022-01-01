6146431fe5b27b6c149b632314ebccf055e056d3
Wailan Tirajoh <wailan.tirajoh@concrete-tech.id>
2022-01-02 01:33:32 +0700

    remove bootstrap vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    // 'bootstrap-vue/nuxt',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
