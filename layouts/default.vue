a7673f583aab7729f0d0d782f5eee1cc0bd42f96
Wailan Tirajoh <wailan.tirajoh@concrete-tech.id>
2022-01-02 01:50:29 +0700

    forgto add default layout to git

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <TheNavbar />
+    <Nuxt class="container" />
+  </div>
+</template>
+
+<script>
+import TheNavbar from "../components/Shared/TheNavbar.vue";
+export default {
+  components: {
+    TheNavbar,
+  },
+};
+</script>
+
+<style>
+</style>
