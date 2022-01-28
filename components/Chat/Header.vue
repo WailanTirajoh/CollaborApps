c743d5ffe6ede2eb34ccd5a159188521de092ada
Wailan Tirajoh <wailan.tirajoh@concrete-tech.id>
2022-01-28 11:16:36 +0700

    change: separate chat header to component

---
+++ components/Chat/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header cursor-pointer" @click="togglePin">
+    <div
+      class="d-flex justify-content-between align-items-center py-2 px-2 w-100"
+    >
+      <div class="d-flex gap-2">
+        <img
+          :src="$auth.user.avatar"
+          class="img-fluid rounded-circle object-fit-cover w-2r h-2r"
+          alt=""
+        />
+        <h6 class="my-auto">Pesan</h6>
+      </div>
+      <div class="me-2">
+        <font-awesome-icon
+          :icon="['fas', isPinned ? 'chevron-down' : 'chevron-up']"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    togglePin() {
+      this.$store.dispatch('chat/updatePin')
+    }
+  }
+}
+</script>
+
+<style></style>
