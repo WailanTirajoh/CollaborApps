1ab2cd5bd959f881d42fbe6e95420bf02a95699c
wisnu setyawan <stephanus.ws@gmail.com>
2022-02-05 01:51:21 +0700

    change : improve style

--- components/Shared/TheChatBox.vue
+++ components/Shared/TheChatBox.vue
@@ -12,7 +12,7 @@
             <ul class="mb-0">
               <li class="clearfix mb-3">
                 <div
-                  class="message-data d-flex justify-content-end align-center"
+                  class="message-data d-flex justify-content-end align-items-center"
                 >
                   <span class="message-data-time mx-2">
                     <span class="fw-bold float-end"> name</span><br />
@@ -21,18 +21,18 @@
                   <img
                     src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
                     class="rounded-circle float-start"
-                    style="height: 40px"
+                    style="height: 35px"
                   />
                 </div>
                 <div class="message my-message float-end">my message</div>
               </li>
 
               <li class="clerfix mb-3">
-                <div class="message-data d-flex align-center">
+                <div class="message-data d-flex align-items-center">
                   <img
                     src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
                     class="rounded-circle float-start"
-                    style="height: 40px"
+                    style="height: 35px"
                   />
                   <span class="message-data-time mx-2">
                     <span class="fw-bold"> name</span> <br />
@@ -43,11 +43,11 @@
               </li>
 
               <li class="clerfix mb-3">
-                <div class="message-data d-flex align-center">
+                <div class="message-data d-flex align-items-center">
                   <img
                     src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
                     class="rounded-circle float-start"
-                    style="height: 40px"
+                    style="height: 35px"
                   />
                   <span class="message-data-time mx-2">
                     <span class="fw-bold"> name</span> <br />
@@ -58,11 +58,11 @@
               </li>
 
               <li class="clerfix mb-3">
-                <div class="message-data d-flex align-center">
+                <div class="message-data d-flex align-items-center">
                   <img
                     src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
                     class="rounded-circle float-start"
-                    style="height: 40px"
+                    style="height: 35px"
                   />
                   <span class="message-data-time mx-2">
                     <span class="fw-bold"> name</span> <br />
@@ -73,11 +73,11 @@
               </li>
 
               <li class="clerfix mb-3">
-                <div class="message-data d-flex align-center">
+                <div class="message-data d-flex align-items-center">
                   <img
                     src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
                     class="rounded-circle float-start"
-                    style="height: 40px"
+                    style="height: 35px"
                   />
                   <span class="message-data-time mx-2">
                     <span class="fw-bold"> name</span> <br />
@@ -122,4 +122,80 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.chat {
+  .chat-header {
+    padding: 15px 20px;
+    border-bottom: 2px solid #f4f7f6;
+  }
+
+  .chat-history {
+    height: 500px;
+    padding: 12px;
+    border-bottom: 2px solid #fff;
+    overflow-x: auto;
+
+    ul {
+      padding: 0;
+
+      li {
+        list-style: none;
+
+        &:last-child {
+          margin-bottom: 0px;
+        }
+      }
+    }
+
+    .message-data {
+      margin-bottom: 4px;
+
+      .message-data-time {
+        color: #434651;
+        font-size: 13px;
+      }
+
+      &.other {
+        justify-content: end;
+      }
+    }
+
+    .message {
+      color: #444;
+      padding: 8px;
+      line-height: 26px;
+      font-size: 16px;
+      border-radius: 4px;
+      display: inline-block;
+      position: relative;
+    }
+
+    .other-message {
+      background: #efefef;
+    }
+
+    .my-message {
+      background: #e8f1f3;
+      text-align: right;
+
+      &::after {
+        border-bottom-color: #e8f1f3;
+        left: 93%;
+      }
+    }
+  }
+
+  .chat-message {
+    padding: 20px;
+  }
+}
+
+.clearfix:after {
+  visibility: hidden;
+  display: block;
+  font-size: 0;
+  content: ' ';
+  clear: both;
+  height: 0;
+}
+</style>
