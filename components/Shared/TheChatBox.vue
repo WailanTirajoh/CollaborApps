2118b478366ee6654d06237798b59cf210b0974a
wisnu setyawan <stephanus.ws@gmail.com>
2022-02-05 09:46:47 +0700

    add : chat API

--- components/Shared/TheChatBox.vue
+++ components/Shared/TheChatBox.vue
@@ -7,99 +7,25 @@
     <ChatHeader />
     <div class="h-100">
       <div class="textbox px-2 py-1" style="overflow-y: auto; height: 80%">
-        <div class="chat">
-          <div class="chat-history">
-            <ul class="mb-0">
-              <li class="clearfix mb-3">
-                <div
-                  class="message-data d-flex justify-content-end align-items-center"
-                >
-                  <span class="message-data-time mx-2">
-                    <span class="fw-bold float-end"> name</span><br />
-                    <span class="fw-lighter">created at</span>
-                  </span>
-                  <img
-                    src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
-                    class="rounded-circle float-start"
-                    style="height: 35px"
-                  />
-                </div>
-                <div class="message my-message float-end">my message</div>
-              </li>
-
-              <li class="clerfix mb-3">
-                <div class="message-data d-flex align-items-center">
-                  <img
-                    src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
-                    class="rounded-circle float-start"
-                    style="height: 35px"
-                  />
-                  <span class="message-data-time mx-2">
-                    <span class="fw-bold"> name</span> <br />
-                    <span class="fw-lighter">created at</span>
-                  </span>
-                </div>
-                <div class="message other-message">other message</div>
-              </li>
-
-              <li class="clerfix mb-3">
-                <div class="message-data d-flex align-items-center">
-                  <img
-                    src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
-                    class="rounded-circle float-start"
-                    style="height: 35px"
-                  />
-                  <span class="message-data-time mx-2">
-                    <span class="fw-bold"> name</span> <br />
-                    <span class="fw-lighter">created at</span>
-                  </span>
-                </div>
-                <div class="message other-message">other message</div>
-              </li>
-
-              <li class="clerfix mb-3">
-                <div class="message-data d-flex align-items-center">
-                  <img
-                    src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
-                    class="rounded-circle float-start"
-                    style="height: 35px"
-                  />
-                  <span class="message-data-time mx-2">
-                    <span class="fw-bold"> name</span> <br />
-                    <span class="fw-lighter">created at</span>
-                  </span>
-                </div>
-                <div class="message other-message">other message</div>
-              </li>
-
-              <li class="clerfix mb-3">
-                <div class="message-data d-flex align-items-center">
-                  <img
-                    src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
-                    class="rounded-circle float-start"
-                    style="height: 35px"
-                  />
-                  <span class="message-data-time mx-2">
-                    <span class="fw-bold"> name</span> <br />
-                    <span class="fw-lighter">created at</span>
-                  </span>
-                </div>
-                <div class="message other-message">other message</div>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <ChatContent v-if="!isLoadingChats" :chats="chats" />
       </div>
       <div>
         <div class="input w-100 position-fixed bottom-0">
           <div class="input-group input-append">
             <input
-              :placeholder="`Masukkan pesan...`"
+              ref="iSendMessage"
+              v-model="message"
+              :disabled="isLoadingSendMessage"
+              placeholder="Masukkan pesan..."
               type="text"
-              autocomplete="off"
               class="form-control"
+              @keyup.enter="addNewChat"
             />
-            <button class="btn btn-primary">
+            <button
+              :disabled="isLoadingSendMessage"
+              class="btn btn-primary"
+              @click="addNewChat"
+            >
               <font-awesome-icon :icon="['fas', 'paper-plane']" />
             </button>
           </div>
@@ -114,88 +40,59 @@
 
 <script>
 export default {
+  data() {
+    return {
+      message: 'hallo',
+      isLoadingSendMessage: false,
+      isLoadingChats: false,
+      chats: []
+    }
+  },
+
+  async fetch() {
+    await this.getChats()
+  },
+
   computed: {
     isPinned() {
       return this.$store.state.chat.isPinned
     }
+  },
+
+  methods: {
+    async addNewChat() {
+      if (!this.message) return
+      try {
+        this.isLoadingSendMessage = true
+        await this.$axios({
+          url: 'chats',
+          method: 'POST',
+          data: {
+            message: this.message
+          }
+        })
+        this.message = ''
+        this.isLoadingSendMessage = false
+        this.$nextTick(() => this.$refs.iSendMessage.focus())
+        this.$fetch()
+      } catch (e) {
+        alert('gagal kirim pesan silahkan coba lagi...')
+      }
+    },
+    async getChats() {
+      try {
+        this.isLoadingChats = false
+        const {
+          data: { chats }
+        } = await this.$axios({
+          url: 'chats',
+          method: 'get'
+        })
+        this.chats = chats
+      } catch (e) {
+        alert('gagal mendapatkan list pesan, silahkan coba lagi...')
+      }
+    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.chat {
-  .chat-header {
-    padding: 15px 20px;
-    border-bottom: 2px solid #f4f7f6;
-  }
-
-  .chat-history {
-    height: 500px;
-    padding: 12px;
-    border-bottom: 2px solid #fff;
-    overflow-x: auto;
-
-    ul {
-      padding: 0;
-
-      li {
-        list-style: none;
-
-        &:last-child {
-          margin-bottom: 0px;
-        }
-      }
-    }
-
-    .message-data {
-      margin-bottom: 4px;
-
-      .message-data-time {
-        color: #434651;
-        font-size: 13px;
-      }
-
-      &.other {
-        justify-content: end;
-      }
-    }
-
-    .message {
-      color: #444;
-      padding: 8px;
-      line-height: 26px;
-      font-size: 16px;
-      border-radius: 4px;
-      display: inline-block;
-      position: relative;
-    }
-
-    .other-message {
-      background: #efefef;
-    }
-
-    .my-message {
-      background: #e8f1f3;
-      text-align: right;
-
-      &::after {
-        border-bottom-color: #e8f1f3;
-        left: 93%;
-      }
-    }
-  }
-
-  .chat-message {
-    padding: 20px;
-  }
-}
-
-.clearfix:after {
-  visibility: hidden;
-  display: block;
-  font-size: 0;
-  content: ' ';
-  clear: both;
-  height: 0;
-}
-</style>
