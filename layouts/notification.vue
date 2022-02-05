4c419ca8936149ec8c697220de9cacbcee9562f8
Wailan Tirajoh <wailantirajoh@gmail.com>
2022-02-05 16:12:20 +0700

    add: notification base

---
+++ layouts/notification.vue
@@ -0,0 +1,28 @@
+<template>
+  <WrapperDefault>
+    <div class="row">
+      <div
+        class="notification col-lg-2 show-desktop px-0 custom-border-right"
+        style="min-height: 100vh"
+      >
+        <ul>
+          <li>
+            <NuxtLink to="/notifications/unread"> Unread </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/notifications/read"> Read </NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-10">
+        <Nuxt />
+      </div>
+    </div>
+  </WrapperDefault>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/notification/read/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="">Unread notification</div>
-</template>
-
-<script>
-export default {}
-</script>
