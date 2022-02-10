cb10f7a1e52ddc7857fb053dc3e88248c1b99568
Wailan Tirajoh <wailan.tirajoh@kawanlamaretail.com>
2022-02-11 00:35:36 +0700

    add: notification unread, style notification

--- pages/notifications/unread/index.vue
+++ pages/notifications/unread/index.vue
@@ -1,5 +1,50 @@
 <template>
-  <div class="row">
+  <div class="notification-detail row">
     <div class="col-lg-12">Unread</div>
+    <div class="col-lg-12">
+      <ul>
+        <li v-for="notification in notifications" :key="notification.id">
+          <div class="user-pic">
+            <img :src="notification.comment.user.avatar" />
+          </div>
+          <div>
+            <div class="user-name">
+              {{ notification.comment.user.name }}
+            </div>
+            <div class="message">
+              {{ notification.comment.message }}
+            </div>
+          </div>
+        </li>
+      </ul>
+      <!-- {{ notifications }} -->
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      notifications: []
+    }
+  },
+  mounted() {
+    this.getNotification()
+  },
+  methods: {
+    async getNotification() {
+      try {
+        const response = await this.$axios.get('/notifications')
+        this.notifications = response.data.notifications
+      } catch (e) {
+        this.$toast
+          .error(e.message, {
+            position: 'top-right',
+            Icon: 'check'
+          })
+          .goAway(2500)
+      }
+    }
+  }
+}
+</script>
